ada2fe0ddb9a798839c7285dc6ff62c02a4c7765
zinnourlahcene <zinnour@uqtr.ca>
2020-06-02 16:49:57 -0400

    Initial commit

---
+++ src/main/java/ca/uqtr/epod/webrtc_service/WebrtcServiceApplication.java
@@ -0,0 +1,13 @@
+package ca.uqtr.epod.webrtc_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrtcServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebrtcServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/uqtr/epod/webrtc_service/WebrtcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.uqtr.epod.webrtc_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrtcServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
